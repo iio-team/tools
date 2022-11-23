7330286a51047c3d186ed568a862e9f8a07da55c
Filippo Casarin <casarin.filippo17@gmail.com>
2022-11-23 19:16:24 +0100

    Update update_tools
    Fix linter warnings

--- update_tools.sh
+++ update_tools.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -e
 
 if ! [[ $(git rev-parse --show-toplevel 2>/dev/null) == "$PWD" ]]; then
     echo "FATAL ERROR: the current folder is not the root of a git repository."
     exit 1
 fi
-if ! [[ "`basename $PWD`" =~ ^iiot-202[0-9]$ ]]; then
+if ! [[ "`basename "$PWD"`" =~ ^iiot-202[0-9]$ ]]; then
     echo "FATAL ERROR: the current folder does not correspond to a IIOT year."
     exit 1
 fi
