745f25825a3d945674ba4402b3cda6ee372e78ed
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2023-02-28 08:59:56 +0100

    Improve validator

--- new_task/validator.py
+++ new_task/validator.py
@@ -15,14 +15,15 @@ def run(f, st):
     for k, v in subtasks[st].items():
         globals()[k] = v
 
-    N = int(f[0])
+    N = int(next(f))
     assert 1 <= N <= MAX_N
 
-    V = list(map(int, f[1].split()))
+    V = list(map(int, next(f).split()))
     assert len(V) == N
     
     assert all(0 <= v <= MAX_V for v in V)
 
+    assert next(f, None) is None
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -34,5 +35,5 @@ if __name__ == "__main__":
     if len(sys.argv) == 3:
         st = int(sys.argv[2])
 
-    f = open(sys.argv[1]).readlines()
+    f = open(sys.argv[1])
     run(f, st)
