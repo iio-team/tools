7967c5053cbd966de82c603d78d870e6dd667a57
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2025-11-04 21:16:40 +0100

    Fix rename_task.sh

--- rename_task.sh
+++ rename_task.sh
@@ -47,7 +47,7 @@ done
 
 # replace in task.yaml
 echo "fixing task.yaml and GEN"
-sed -i "s/name: $1/name: $2/" task.yaml
+sed -i "s/name: $1/name: $2/" task.yaml.orig
 sed -i "s/$1.input/$2.input/" gen/GEN
 
 # fix contest.yaml
